97e953b800f02ae306d61f265e4fb411ec99cad9
A7X-lab <134360870+A7X-lab@users.noreply.github.com>
2023-06-24 22:49:42 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    const double Rate = 0;
+    const double initialAmount = 0;
+    const double TRGET = 2 * initialAmount;
+
+    double Amount = initialAmount;
+    int year = 0;
+
+    while (balance < Target)
+        (
+                year++
+                double interest = balance + Rate/ 100;
+                balance = balance + interest;
+        )
+
+        cout << "The investment is"
+                )
+
+    return 0;
+}
